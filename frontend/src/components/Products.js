28987d284bedf906df9afc451cd32fc2712d2460
Namuk nazu <nazu@Namuks-MacBook-Air.local>
2026-01-12 18:00:23 +0800

    edit

--- frontend/src/components/Products.js
+++ frontend/src/components/Products.js
@@ -181,7 +181,7 @@ export const Products = () => {
         {/* PRODUCT DETAIL MODAL */}
         {selectedProduct && (
           <div className="fixed inset-0 bg-black/50 flex items-center justify-center p-4 z-50">
-            <div className="bg-white rounded-2xl max-w-xl w-full p-6 relative mt-15 max-h-screen overflow-auto">
+            <div className="bg-white rounded-2xl max-w-xl w-full p-6 relative mt-10">
               <button
                 onClick={() => {
                   setSelectedProduct(null);
@@ -212,13 +212,15 @@ export const Products = () => {
                   />
                 ))}
               </div>
-
-              <h2 className="text-2xl font-bold">{selectedProduct.name}</h2>
-              <p className="text-gray-700">{selectedProduct.description}</p>
-              <p className="text-red-500 font-bold mt-2">
-                {selectedProduct.price}₮
-              </p>
-
+              <div className="flex">
+                <div>
+                  <h2 className="text-2xl font-bold">{selectedProduct.name}</h2>
+                  <p className="text-gray-700">{selectedProduct.description}</p>
+                </div>
+                <p className="text-red-500 font-bold mt-2">
+                  {selectedProduct.price}₮
+                </p>
+              </div>
               {/* Үнэр сонгох */}
               {selectedProduct.category === "лаа" && (
                 <div className="mb-4">
